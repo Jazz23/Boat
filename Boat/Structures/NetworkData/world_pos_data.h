ef2c68f8280b7e3b61e7afb49ec647b3a6075f9b
Iizerd <jameswilliammcgowan@gmail.com>
2019-08-09 11:45:59 -0700

    dwaf
    wd

--- Boat/Structures/NetworkData/world_pos_data.h
+++ Boat/Structures/NetworkData/world_pos_data.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <math.h>
 #include "../Connection/PacketBuffer.h"
-class WorldPosData
+#include "../../Connection/Packets.h"
+class WorldPosData : Packet::BasePacket
 {
 public:
 	float x, y;
@@ -12,13 +13,13 @@ public:
 	{
 		return pow(pow((double)(location.x) - x, 2) + pow((double)(location.y) - y, 2), 0.5); //vs wanted me to cast it to a double before subtracting for some reason...
 	}
-	void Read(Packet::PacketBuffer* pb)
+	void ReadData(Packet::PacketBuffer* pb) override
 	{
 		if (!pb) return;
 		x = pb->ReadFloat();
 		y = pb->ReadFloat();
 	}
-	void Write(Packet::PacketBuffer* pb)
+	void WriteData(Packet::PacketBuffer* pb) override
 	{
 		if (!pb) return;
 		pb->WriteFloat(x);
